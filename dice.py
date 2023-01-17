1650d40458a48dab78493b5b341add7d224d778e
dhruvsinghal7494 <122340013+dhruvsinghal7494@users.noreply.github.com>
2023-01-17 16:42:55 +0530

    Update dice.py

--- dice.py
+++ dice.py
@@ -1,8 +1,8 @@
 import random
 
 def roll_dice(n):
-    results = {7,8,9,10,11,12}
-    a = 0
+    results = {}
+    a = 
     for i in range(n):
         dice1 = random.randint(1, 6)
         dice2 = random.randint(1, 6)
@@ -16,7 +16,7 @@ def roll_dice(n):
 if __name__ == "__main__":
     n = int(input("Enter the number of times to roll the dice: "))
     a = roll_dice(n)
-    #for i in range(7, 12):
+    for i in range(2, 12):
         #print(f"Sum {i}: {results.get(i, 0)}")
 print(a)
 
